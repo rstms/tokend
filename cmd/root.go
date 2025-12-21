979af9e3382fa01d94e18b94083986ed75a91999
Matt Krueger <mkrueger@rstms.net>
2025-12-21 08:09:40 -0700

    Bump version: 0.1.4 → 0.1.5

--- cmd/root.go
+++ cmd/root.go
@@ -40,7 +40,7 @@ import (
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Version: "0.1.4",
+	Version: "0.1.5",
 	Use:     "tokend",
 	Short:   "OAUTH2 backend and token mananagement daemon",
 	Long: `
